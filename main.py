c865fd242f9c0e5fee9eae903e63068c5a322174
Ryder Sharpe <125460623+RyderSharpe@users.noreply.github.com>
2024-11-19 21:36:43 -0800

    main.py

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
-import smtplib
+# import smtplib
+#
+# my_email = "rhyder.sharpe@gmail.com"
+# # my_other_email = "rhyder.sharpe@yahoo.com"
+# password = "desqcbrcqnjkrxwc"
+#
+#
+# with smtplib.SMTP("smtp.gmail.com") as connection_gmail:
+#     # connection_yahoo = smtplib.SMTP("smtp.mail.yahoo.com")
+#     connection_gmail.starttls() # Secure connection
+#     connection_gmail.login(user=my_email, password=password)
+#     connection_gmail.sendmail(
+#         from_addr=my_email,
+#         to_addrs="rhyder.sharpe@yahoo.com",
+#         msg="Subject:Hello\n\nThis is the content of my email. does this even work?"
+#     )
+#
+# # connection_gmail.close()
 
-my_email = "rhyder.sharpe@gmail.com"
-# my_other_email = "rhyder.sharpe@yahoo.com"
-password = "desqcbrcqnjkrxwc"
+import datetime as dt
+
+now = dt.datetime.now()
+year = now.year
+month = now.month
+day = now.day
+hour = now.hour
+day_of_week = now.weekday()
 
 
-with smtplib.SMTP("smtp.gmail.com") as connection_gmail:
-    # connection_yahoo = smtplib.SMTP("smtp.mail.yahoo.com")
-    connection_gmail.starttls() # Secure connection
-    connection_gmail.login(user=my_email, password=password)
-    connection_gmail.sendmail(
-        from_addr=my_email,
-        to_addrs="rhyder.sharpe@yahoo.com",
-        msg="Subject:Hello\n\nThis is the content of my email"
-    )
-
-
-# connection_gmail.close()
+date_of_birth = dt.datetime(year=1990, month=5, day=5, hour=11, minute=11)
+print(date_of_birth)
